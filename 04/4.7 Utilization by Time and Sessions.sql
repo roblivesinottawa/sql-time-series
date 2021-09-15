6ca4b5e76fe9f0284a88824fce02b03cb1737a9c
Robson Ferreira <tech.rob@icloud.com>
2021-09-15 06:35:12 -0400

    initial commit

---
+++ 04/4.7 Utilization by Time and Sessions.sql
@@ -0,0 +1,28 @@
+﻿EXPLAIN SELECT server_id, avg(cpu_utilization)
+FROM time_series.utilization
+WHERE event_time BETWEEN '2019-03-05' AND '2019-03-06'
+GROUP BY server_id;
+
+
+CREATE INDEX idx_util_time_serv ON utilization(event_time,server_id);
+
+
+EXPLAIN SELECT server_id, avg(cpu_utilization)
+FROM time_series.utilization
+WHERE event_time BETWEEN '2019-03-05' AND '2019-03-06'
+GROUP BY server_id;
+
+
+DROP INDEX idx_util_time_serv;
+
+
+CREATE INDEX dx_util_time_serv ON utilization(server_id,event_time);
+
+
+EXPLAIN SELECT server_id, avg(cpu_utilization)
+FROM time_series.utilization
+WHERE event_time BETWEEN '2019-03-05' AND '2019-03-06'
+GROUP BY server_id;
+
+
+DROP INDEX  idx_util_time_serv;
